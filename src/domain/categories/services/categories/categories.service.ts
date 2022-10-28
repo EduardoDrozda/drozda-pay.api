92efb78953368ba9c5cff5654be20e6e3d209182
Eduardo Drozda <Edu_Drozda@hotmail.com>
2022-10-28 15:17:04 -0300

    feat: addo method createDefaultCategories when new user is created

--- src/domain/categories/services/categories/categories.service.ts
+++ src/domain/categories/services/categories/categories.service.ts
@@ -4,6 +4,26 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CategoriesService {
+  private readonly DEFAULT_CATEGORIES: CreateCategoryDto[] = [
+    {
+      name: 'Entradas',
+      description: 'Categoria para valores que somam no saldo final',
+    },
+    {
+      name: 'Saídas',
+      description: 'Categoria para valores que diminuem o seu saldo final',
+    },
+    {
+      name: 'Investimentos',
+      description: 'Categoria para valores que você investe',
+    },
+  ];
+
+  createDefaultCategories(userId: number) {
+    this.DEFAULT_CATEGORIES.forEach((category) => {
+      this.create({ ...category, userId });
+    });
+  }
   constructor(private readonly categoriesRepository: CategoriesRepository) {}
   async create(data: CreateCategoryDto) {
     return await this.categoriesRepository.create(data);
--- src/domain/user/user.module.ts
+++ src/domain/user/user.module.ts
@@ -1,9 +1,11 @@
+import { CategoriesModule } from '../categories';
 import { Module } from '@nestjs/common';
 import { UserController } from './controllers/user/user.controller';
 import { UserRepository } from './repositories';
 import { UserService } from './services/user/user.service';
 
 @Module({
+  imports: [CategoriesModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
   exports: [UserService, UserRepository],
