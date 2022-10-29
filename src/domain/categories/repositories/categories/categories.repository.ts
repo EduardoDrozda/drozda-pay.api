3f5dd724882d744a6094694cdbc233c0ce01af43
Eduardo Drozda <Edu_Drozda@hotmail.com>
2022-10-29 15:41:27 -0300

    feat: create category crud

--- src/domain/categories/repositories/categories/categories.repository.ts
+++ src/domain/categories/repositories/categories/categories.repository.ts
@@ -1,12 +1,48 @@
+import { Category, Prisma } from '@prisma/client';
+
+import { CreateCategoryDto } from '../../dtos';
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
 import { PrismaAdapter } from 'src/shared/adapters';
 
 @Injectable()
 export class CategoriesRepository extends PrismaAdapter {
   async create(data: Prisma.CategoryCreateInput) {
-    return this.category.create({
+    return await this.category.create({
       data,
+    });
+  }
+
+  async findAllByUserId(userId: number) {
+    return await this.category.findMany({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  async findById(id: number, userId: number) {
+    return await this.category.findFirst({
+      where: {
+        id,
+        userId,
+      },
+    });
+  }
+
+  async update(id: number, category: CreateCategoryDto) {
+    return await this.category.update({
+      where: {
+        id,
+      },
+      data: category,
+    });
+  }
+
+  async delete(id: number) {
+    return await this.category.delete({
+      where: {
+        id,
+      },
     });
   }
 }
